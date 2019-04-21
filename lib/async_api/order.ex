c81b0cfe604fd1c03fd0358c3c2d69de05e4f3e9
IlyaOsotov <soten58@yandex.ru>
2019-04-21 15:45:11 +0300

    added order model

---
+++ lib/async_api/order.ex
@@ -0,0 +1,17 @@
+defmodule Order do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias AsyncApi.Order
+
+  schema "orders" do
+    field :content, :string
+    field :status, :string
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:content, :status])
+  end
+end
--- priv/repo/migrations/20190421123813_add_orders.exs
+++ priv/repo/migrations/20190421123813_add_orders.exs
@@ -2,6 +2,11 @@ defmodule AsyncApi.Repo.Migrations.AddOrders do
   use Ecto.Migration
 
   def change do
+    create table(:orders) do
+      add :content, :string
+      add :status, :string
 
+      timestamps()
+    end
   end
 end
